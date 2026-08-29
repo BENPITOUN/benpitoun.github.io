5a2f54b79c5fc842cffef5c791dcf61b9e3ca71d
BENPITOUN <105542526+BENPITOUN@users.noreply.github.com>
2026-08-29 23:08:46 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,13 +4,16 @@ window.WEDDING_LINKS = {
     best: "https://1drv.ms/v/c/fe005e79bfca4940/IQA9j2LU069tQZHTK4iZlwjrATEVMWPv9kOVrE483ljqejA?e=RYAW4C",
     teaser: "https://1drv.ms/v/c/fe005e79bfca4940/IQAjCyGfygCZTqTep9M6bsvCAYCxd21ZL0XIhsGJO7BipLE?e=xhvKRP"
   },
+
   photos: {
-    mairie: "https://1drv.ms/f/c/fe005e79bfca4940/IgDsOcsWGvr4RryYZds08DW2AawUG6kRzkuTlXxWUFYZLFs?e=v3ersY",
-    pool: "https://1drv.ms/f/c/fe005e79bfca4940/IgDuptUJkQZZSZ4AB_QsBYPZAZgbjmTPhTsS5VreP3X74Gw?e=dByM7w",
-    ceremonie: "https://1drv.ms/f/c/fe005e79bfca4940/IgADaAe-J7v5Q7nJpM8TXW8RAULHEfwfPqvIj4obEGq2BbI?e=ILHmum",
-    maries: "https://1drv.ms/f/c/fe005e79bfca4940/IgC6qksjRPYsRqcmyRv9aLfeARXlUR36wIG08EQ1GsDMVnc?e=84UQQE",
-    cocktail: "https://1drv.ms/f/c/fe005e79bfca4940/IgDQ3V_iCFh2Q7K-QLQauPHvAfO5v2696QHn81lg4MkHLmc?e=0gXGG2"
+    mairie: "LIEN_AMAZON_MAIRIE",
+    pool: "LIEN_AMAZON_BRUNCH_POOL_PARTY",
+    henne: "LIEN_AMAZON_HENNE",
+    ceremonie: "LIEN_AMAZON_CEREMONIE",
+    maries: "LIEN_AMAZON_LES_MARIES",
+    cocktail: "LIEN_AMAZON_COCKTAIL_SOIREE"
   },
+
   bonus: {
     adjointe: "https://1drv.ms/v/c/fe005e79bfca4940/IQC60GVEB7knQZflV5qRnCfxAbYD88Ypl5VMBLUPHVLP2HA?e=sA8LEf",
     rabbin: "https://1drv.ms/v/c/fe005e79bfca4940/IQDqzyZx8Nz5SoJObL8eiVc0Acm5nAv7ZCgJnDCNC0UUDbM?e=Fjuq3d",
