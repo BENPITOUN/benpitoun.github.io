62b32cf5622f292baa642415e9ad49af42b55a6e
BENPITOUN <105542526+BENPITOUN@users.noreply.github.com>
2026-08-30 15:45:21 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,23 @@
-Unsupported Media Type
+window.WEDDING_LINKS = {
+  films: {
+    full: "https://1drv.ms/v/c/fe005e79bfca4940/IQBIdTJ2O6qjRb9RRKXOKWN0ATMLtKTwSLD7HVVD_84lqXA?e=1VzArn",
+    best: "https://1drv.ms/v/c/fe005e79bfca4940/IQA9j2LU069tQZHTK4iZlwjrATEVMWPv9kOVrE483ljqejA?e=RYAW4C",
+    teaser: "https://1drv.ms/v/c/fe005e79bfca4940/IQAjCyGfygCZTqTep9M6bsvCAYCxd21ZL0XIhsGJO7BipLE?e=xhvKRP"
+  },
+
+  photos: {
+    mairie: "https://www.amazon.fr/photos/share/n4XJWVa9qbsG0tH6KJ70A1rgJAYkYicf8KU6TisNVqP",
+    pool: "https://www.amazon.fr/photos/share/pqCMcqH98RvIIFrXX5YHV3gk5K0L9kUsGllHJpJdzyE",
+    henne: "https://www.amazon.fr/photos/share/iC0X4VExMd8LE61go6mnQo1NGTknw9wWbCkyVOMqxE8",
+    ceremonie: "https://www.amazon.fr/photos/share/IkVGYYPBdqm6L0aoGTlIHhMTGRx8wFAXFjWb169wyAQ",
+    maries: "https://www.amazon.fr/photos/share/qdJrQpHkRMk6bMGdoES0JgvgCn8qwiXUAECP8Tq5O0w",
+    cocktail: "https://www.amazon.fr/photos/share/c50P9JeWukgFVI6ACFIiTuJ3D4bVUFm8YXMRtw8utIT"
+  },
+
+  bonus: {
+    adjointe: "https://1drv.ms/v/c/fe005e79bfca4940/IQC60GVEB7knQZflV5qRnCfxAbYD88Ypl5VMBLUPHVLP2HA?e=sA8LEf",
+    rabbin: "https://1drv.ms/v/c/fe005e79bfca4940/IQDqzyZx8Nz5SoJObL8eiVc0Acm5nAv7ZCgJnDCNC0UUDbM?e=Fjuq3d",
+    ketouba: "https://1drv.ms/v/c/fe005e79bfca4940/IQBhnemkNUCyTrfuvbTE9nVgAfYkqxFQ_273QRnzHiIqBp0?e=B9DHrr",
+    tango: "https://1drv.ms/v/c/fe005e79bfca4940/IQBwHuazdbvsRYAT9TpMJFJ-AYJU199sYpebnge30X5Ig9M?e=r0Mjl2"
+  }
+};
